22cd5c4de79db1eee69c6db13c370d0689be3dfa
vnikita <vnikita123456@gmail.com>
2021-10-08 02:22:08 +0700

    1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace Vasilev3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Исходное время:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(404, 75);
+            this.label5.TabIndex = 7;
+            this.label5.Text = resources.GetString("label5.Text");
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(90, 116);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Оператор";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 97);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(36, 19);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "!=";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(33, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "+";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 72);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(41, 19);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "==";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 47);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(30, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "-";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Значение";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(276, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 15);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Результат";
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(264, 30);
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.ReadOnly = true;
+            this.maskedTextBox3.Size = new System.Drawing.Size(90, 23);
+            this.maskedTextBox3.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(27, 23);
+            this.textBox1.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(139, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 15);
+            this.label4.TabIndex = 19;
+            this.label4.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(188, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(10, 15);
+            this.label6.TabIndex = 20;
+            this.label6.Text = ":";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(27, 23);
+            this.textBox2.TabIndex = 21;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(204, 30);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(27, 23);
+            this.textBox3.TabIndex = 22;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(108, 73);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(123, 23);
+            this.textBox4.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(237, 76);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(140, 45);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "- время в секундах, \r\nлибо в формате \r\n\"часы:минуты:секунды\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 223);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vasilev3
+{
+    public partial class Form1 : Form
+    {
+        private string TimeMask = "00:00:00";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.MaxLength = 2;
+            textBox2.MaxLength = 2;
+            textBox3.MaxLength = 2;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyTime Test = new MyTime();
+            try
+            {
+                Test.Hours = Convert.ToInt32(textBox1.Text);
+                Test.Minutes = Convert.ToInt32(textBox2.Text);
+                Test.Seconds = Convert.ToInt32(textBox3.Text);
+                if (radioButton1.Checked)
+                {
+                    int plusSeconds;
+                    bool numCheck = int.TryParse(textBox4.Text, out plusSeconds);
+                    if (numCheck)
+                    {
+                        Test = Test + plusSeconds;
+                    }
+                    else
+                    {
+                        string[] times = textBox4.Text.Split(":");
+                        MyTime Test2 = new MyTime();
+                        Test2.Hours = Convert.ToInt32(times[0]);
+                        Test2.Minutes = Convert.ToInt32(times[1]);
+                        Test2.Seconds = Convert.ToInt32(times[2]);
+                        Test = Test + Test2;
+                    }
+
+                    string resHours = Test.Hours.ToString();
+                    string resMinutes = Test.Minutes.ToString();
+                    string resSeconds = Test.Seconds.ToString();
+
+                    if (resSeconds.Length == 1)
+                    {
+                        resSeconds = "0" + resSeconds;
+                    }
+                    if (resMinutes.Length == 1)
+                    {
+                        resMinutes = "0" + resMinutes;
+                    }
+                    if (resHours.Length == 1)
+                    {
+                        resHours = "0" + resHours;
+                    }
+
+                    maskedTextBox3.Text = $"{resHours}:{resMinutes}:{resSeconds}";
+                }
+                if (radioButton2.Checked)
+                {
+                    int plusSeconds;
+                    bool numCheck = int.TryParse(textBox4.Text, out plusSeconds);
+                    if (numCheck)
+                    {
+                        Test = Test - plusSeconds;
+                    }
+                    else
+                    {
+                        string[] times = textBox4.Text.Split(":");
+                        MyTime Test2 = new MyTime();
+                        Test2.Hours = Convert.ToInt32(times[0]);
+                        Test2.Minutes = Convert.ToInt32(times[1]);
+                        Test2.Seconds = Convert.ToInt32(times[2]);
+                        Test = Test - Test2;
+                    }
+
+                    string resHours = Test.Hours.ToString();
+                    string resMinutes = Test.Minutes.ToString();
+                    string resSeconds = Test.Seconds.ToString();
+
+                    if (resSeconds.Length == 1)
+                    {
+                        resSeconds = "0" + resSeconds;
+                    }
+                    if (resMinutes.Length == 1)
+                    {
+                        resMinutes = "0" + resMinutes;
+                    }
+                    if (resHours.Length == 1)
+                    {
+                        resHours = "0" + resHours;
+                    }
+
+                    maskedTextBox3.Text = $"{resHours}:{resMinutes}:{resSeconds}";
+                }
+                if (radioButton3.Checked)
+                {
+                    string[] times = textBox4.Text.Split(":");
+                    MyTime Test2 = new MyTime();
+                    Test2.Hours = Convert.ToInt32(times[0]);
+                    Test2.Minutes = Convert.ToInt32(times[1]);
+                    Test2.Seconds = Convert.ToInt32(times[2]);
+                    bool check = false;
+                    if (Test == Test2)
+                        check = true;
+                    maskedTextBox3.Text = check.ToString();
+                }
+                if (radioButton4.Checked)
+                {
+                    string[] times = textBox4.Text.Split(":");
+                    MyTime Test2 = new MyTime();
+                    Test2.Hours = Convert.ToInt32(times[0]);
+                    Test2.Minutes = Convert.ToInt32(times[1]);
+                    Test2.Seconds = Convert.ToInt32(times[2]);
+                    bool check = false;
+                    if (Test != Test2)
+                        check = true;
+                    maskedTextBox3.Text = check.ToString();
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Неверные значения", "Ошибка");
+            }
+
+          
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class MyTime
+    {
+        internal int Seconds;
+        internal int Minutes;
+        internal int Hours;
+
+        public MyTime() { }
+
+        public static MyTime operator +(MyTime time, int Second)
+        {
+            while (Second > 59)
+            {
+                time.Minutes += 1;
+                Second -= 60;
+                if (time.Minutes > 59)
+                {
+                    time.Hours += 1;
+                    time.Minutes -= 60;
+                    if (time.Hours > 23)
+                    {
+                        time.Hours = 0;
+                    }
+                }
+            }
+            if (time.Seconds + Second >=60)
+            {
+                time.Minutes += 1;
+                time.Seconds += Second;
+                time.Seconds -= 60;
+                if (time.Minutes > 59)
+                {
+                    time.Hours += 1;
+                    time.Minutes -= 60;
+                    if (time.Hours > 23)
+                    {
+                        time.Hours = 0;
+                    }
+                }
+            }
+            else
+            {
+                time.Seconds += Second;
+            }
+            return time;
+        }
+        public static MyTime operator +(MyTime time, MyTime time2)
+        {
+            time.Seconds += time2.Seconds;
+            time.Minutes += time2.Minutes;
+            time.Hours += time2.Hours;
+            while (time.Seconds > 59)
+            {
+                time.Minutes += 1;
+                time.Seconds -= 60;
+            }
+            while (time.Minutes > 59)
+            {
+                time.Hours += 1;
+                time.Minutes -= 60;
+                
+            }
+            while (time.Hours > 23)
+            {
+                time.Hours -= 24;
+            }
+            
+            return time;
+        }
+        public static MyTime operator -(MyTime time, int Second)
+        {
+            time.Seconds -= Second;
+            while (time.Seconds < 0)
+            {
+                time.Minutes -= 1;
+                time.Seconds += 60;
+            }
+            while (time.Minutes < 0)
+            {
+                time.Hours -= 1;
+                time.Minutes += 60;
+            }
+            while (time.Hours < 0)
+            {
+                time.Hours += 24;
+            }
+            return time;
+        }
+        public static MyTime operator -(MyTime time, MyTime time2)
+        {
+            time.Seconds -= time2.Seconds;
+            time.Minutes -= time2.Minutes;
+            time.Hours -= time2.Hours;
+            while (time.Seconds < 0)
+            {
+                time.Minutes -= 1;
+                time.Seconds += 60;
+            }
+            while (time.Minutes < 0)
+            {
+                time.Hours -= 1;
+                time.Minutes += 60;
+            }
+            while (time.Hours < 0)
+            {
+                time.Hours += 24;
+            }
+
+            return time;
+        }
+        public static bool operator ==(MyTime time, MyTime time2)
+        {
+            if (time.Seconds == time2.Seconds && time.Minutes == time2.Minutes && time.Hours == time2.Hours)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(MyTime time, MyTime time2)
+        {
+            if (time.Seconds != time2.Seconds || time.Minutes != time2.Minutes || time.Hours != time2.Hours)
+                return true;
+            else
+                return false;
+        }
+    }
+}
